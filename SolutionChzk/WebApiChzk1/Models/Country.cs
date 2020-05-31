d9d92eee1e556732c2184eb9c5513e7866c25eb5
Robert <Robert@ROBERT-ASUSROG>
2020-05-30 23:04:42 -0500

    insert colombia

--- SolutionChzk/WebApiChzk1/Models/Country.cs
+++ SolutionChzk/WebApiChzk1/Models/Country.cs
@@ -12,7 +12,7 @@ namespace WebApiChzk1.Models
     using System;
     using System.Collections.Generic;
     
-    //Paises: perú, argentina, mexino
+    //Paises: perú, argentina, mexico, colombia
     public partial class Country
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
