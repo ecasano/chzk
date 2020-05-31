06ef22e8ff8c218e274ae36753b54af7fb67028a
Eduardo Raul Casaño Arzapalo <ecasano@gmail.com>
2020-05-31 10:00:34 -0500

    country

---
+++ SolutionChzk/WebApiChzk1/Models/CountrySOA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiChzk1.Models
+{
+    public partial class Country
+    {
+        chzkEntities db = new chzkEntities();
+
+        
+        //  alumno alumno = db.alumnos.Find(this.id);
+        //  alumno.edad += 1;
+        //  db.Entry(alumno).State = EntityState.Modified;
+        //  db.SaveChanges();
+
+    }
+}
